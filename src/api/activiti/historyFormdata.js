d1b8e3cace448a093bdf1d4be78fafc7d459abf7
PCMICRO-2J5ADRN\danny <zhao_402295440@126.com>
2021-02-07 00:56:07 +0800

    代码回滚

--- src/api/activiti/historyFormdata.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-// 查询请假详细
-export function historyFromData(instanceId) {
-  return request({
-    url: '/historyFromData/ByInstanceId/' + instanceId,
-    method: 'get'
-  })
-}
